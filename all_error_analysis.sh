32c64dc5f445c78f104031d2c96e205bade06061
Eli Goldner <egoldner@brandeis.edu>
2024-04-10 14:35:51 -0400

    So I only have to do it once

---
+++ all_error_analysis.sh
@@ -0,0 +1,75 @@
+# Melanoma
+## Strict
+python summarize_evaluate_analyze.py --gold_path ~/HDD/DockerReadyTimelinesData/test/gold_timelines/melanoma_test_anafora_gold_timelines.json \
+       --input_tsv ../deepphe_timelines_open/subtask1_test_timelines/melanoma.tsv \
+       --output_dir subtask1_test_error_analysis/melanoma/day/ \
+       --all_id_path ~/HDD/DockerReadyTimelinesData/test/all_patient_notes/patient_ids/melanoma_test_patient_ids.txt \
+       --strict
+## Day
+python summarize_evaluate_analyze.py --gold_path ~/HDD/DockerReadyTimelinesData/test/gold_timelines/melanoma_test_anafora_gold_timelines.json \
+       --input_tsv ../deepphe_timelines_open/subtask1_test_timelines/melanoma.tsv \
+       --output_dir subtask1_test_error_analysis/melanoma/day/ \
+       --all_id_path ~/HDD/DockerReadyTimelinesData/test/all_patient_notes/patient_ids/melanoma_test_patient_ids.txt \
+       --relaxed_to day
+## Month
+python summarize_evaluate_analyze.py --gold_path ~/HDD/DockerReadyTimelinesData/test/gold_timelines/melanoma_test_anafora_gold_timelines.json \
+       --input_tsv ../deepphe_timelines_open/subtask1_test_timelines/melanoma.tsv \
+       --output_dir subtask1_test_error_analysis/melanoma/month/ \
+       --all_id_path ~/HDD/DockerReadyTimelinesData/test/all_patient_notes/patient_ids/melanoma_test_patient_ids.txt \
+       --relaxed_to month
+## Year
+python summarize_evaluate_analyze.py --gold_path ~/HDD/DockerReadyTimelinesData/test/gold_timelines/melanoma_test_anafora_gold_timelines.json \
+       --input_tsv ../deepphe_timelines_open/subtask1_test_timelines/melanoma.tsv \
+       --output_dir subtask1_test_error_analysis/melanoma/year/ \
+       --all_id_path ~/HDD/DockerReadyTimelinesData/test/all_patient_notes/patient_ids/melanoma_test_patient_ids.txt \
+       --relaxed_to year
+# Breast
+## Strict
+python summarize_evaluate_analyze.py --gold_path ~/HDD/DockerReadyTimelinesData/test/gold_timelines/breast_test_anafora_gold_timelines.json \
+       --input_tsv ../deepphe_timelines_open/subtask1_test_timelines/breast.tsv \
+       --output_dir subtask1_test_error_analysis/breast/day/ \
+       --all_id_path ~/HDD/DockerReadyTimelinesData/test/all_patient_notes/patient_ids/breast_test_patient_ids.txt \
+       --strict
+## Day
+python summarize_evaluate_analyze.py --gold_path ~/HDD/DockerReadyTimelinesData/test/gold_timelines/breast_test_anafora_gold_timelines.json \
+       --input_tsv ../deepphe_timelines_open/subtask1_test_timelines/breast.tsv \
+       --output_dir subtask1_test_error_analysis/breast/day/ \
+       --all_id_path ~/HDD/DockerReadyTimelinesData/test/all_patient_notes/patient_ids/breast_test_patient_ids.txt \
+       --relaxed_to day
+## Month
+python summarize_evaluate_analyze.py --gold_path ~/HDD/DockerReadyTimelinesData/test/gold_timelines/breast_test_anafora_gold_timelines.json \
+       --input_tsv ../deepphe_timelines_open/subtask1_test_timelines/breast.tsv \
+       --output_dir subtask1_test_error_analysis/breast/month/ \
+       --all_id_path ~/HDD/DockerReadyTimelinesData/test/all_patient_notes/patient_ids/breast_test_patient_ids.txt \
+       --relaxed_to month
+## Year
+python summarize_evaluate_analyze.py --gold_path ~/HDD/DockerReadyTimelinesData/test/gold_timelines/breast_test_anafora_gold_timelines.json \
+       --input_tsv ../deepphe_timelines_open/subtask1_test_timelines/breast.tsv \
+       --output_dir subtask1_test_error_analysis/breast/year/ \
+       --all_id_path ~/HDD/DockerReadyTimelinesData/test/all_patient_notes/patient_ids/breast_test_patient_ids.txt \
+       --relaxed_to year
+# Ovarian
+## Strict
+python summarize_evaluate_analyze.py --gold_path ~/HDD/DockerReadyTimelinesData/test/gold_timelines/ovarian_test_anafora_gold_timelines.json \
+       --input_tsv ../deepphe_timelines_open/subtask1_test_timelines/ovarian.tsv \
+       --output_dir subtask1_test_error_analysis/ovarian/day/ \
+       --all_id_path ~/HDD/DockerReadyTimelinesData/test/all_patient_notes/patient_ids/ovarian_test_patient_ids.txt \
+       --strict
+## Day
+python summarize_evaluate_analyze.py --gold_path ~/HDD/DockerReadyTimelinesData/test/gold_timelines/ovarian_test_anafora_gold_timelines.json \
+       --input_tsv ../deepphe_timelines_open/subtask1_test_timelines/ovarian.tsv \
+       --output_dir subtask1_test_error_analysis/ovarian/day/ \
+       --all_id_path ~/HDD/DockerReadyTimelinesData/test/all_patient_notes/patient_ids/ovarian_test_patient_ids.txt \
+       --relaxed_to day
+## Month
+python summarize_evaluate_analyze.py --gold_path ~/HDD/DockerReadyTimelinesData/test/gold_timelines/ovarian_test_anafora_gold_timelines.json \
+       --input_tsv ../deepphe_timelines_open/subtask1_test_timelines/ovarian.tsv \
+       --output_dir subtask1_test_error_analysis/ovarian/month/ \
+       --all_id_path ~/HDD/DockerReadyTimelinesData/test/all_patient_notes/patient_ids/ovarian_test_patient_ids.txt \
+       --relaxed_to month
+## Year
+python summarize_evaluate_analyze.py --gold_path ~/HDD/DockerReadyTimelinesData/test/gold_timelines/ovarian_test_anafora_gold_timelines.json \
+       --input_tsv ../deepphe_timelines_open/subtask1_test_timelines/ovarian.tsv \
+       --output_dir subtask1_test_error_analysis/ovarian/year/ \
+       --all_id_path ~/HDD/DockerReadyTimelinesData/test/all_patient_notes/patient_ids/ovarian_test_patient_ids.txt \
+       --relaxed_to year
